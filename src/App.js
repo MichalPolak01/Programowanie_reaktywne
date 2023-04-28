94019c46b1be1b267190ed91f6c37aa87790f6c6
Michał Polak <michalpl618@gmail.com>
2023-04-28 16:22:48 +0200

    Lab7

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import QuizComponent from './components/QuizComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Test1 from './components/1-componentClass';
+// import Test2 from './components/2-componentFunctional';
+
+function App() {
+  return (
+    <QuizComponent />
+  );
+}
+
+export default App;
