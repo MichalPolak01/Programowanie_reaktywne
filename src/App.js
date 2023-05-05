ba2f90e0e65929dd5b6b1f3ed9302a8da1427ba3
Michał Polak <michalpl618@gmail.com>
2023-05-05 12:58:40 +0200

    lab8

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Posts from './components/posts';
+
+function App() {
+  return (
+      <main className='contrainer'>
+          <Posts />
+      </main>
+  );
+}
+
+export default App;
